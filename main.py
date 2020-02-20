90022705df82dd67ba8bd427cbab144838bda2a7
astlive <irandomsky@gmail.com>
2020-02-20 17:33:04 +0800

    Select Camera

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def set_res(cap, x,y):
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, int(y))
     return str(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),str(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-def cap_select(cam):
+def cam_check(cam):
     cap = cv2.VideoCapture(cam, cv2.CAP_DSHOW)
+    frame_count = 0
     if(cap is None or not cap.isOpened()):
         return False
     size = set_res(cap, 640,480)
-    fps = cap.get(cv2.CAP_PROP_FPS)
     key = ''
-    print("Camera:" + str(cam) + " FPS:" + str(fps) + " Size:" + str(size))
+    print("Camera:" + str(cam) + " Size:" + str(size))
     while(True):
         ret, frame = cap.read()
         cv2.imshow("Camera:" + str(cam) + " Press \'Y\' to Select or Press \'N\' to ignore", frame)
@@ -29,12 +29,12 @@ def cap_select(cam):
         return True
     return False
 
-def main():
+def cap_select():
     cam_left_num = -1
     cam_right_num = -1
     print("Select Camera Left(1-10):")
     for cam_num in range(1,10):
-        cam_sel = cap_select(cam_num)
+        cam_sel = cam_check(cam_num)
         if(cam_sel == True):
             cam_left_num = cam_num
             print("Select " + str(cam_left_num) + " As Camera Left(<<)")
@@ -46,15 +46,14 @@ def main():
     for cam_num in range(1,10):
         if(cam_num == cam_left_num):
             continue
-        cam_sel = cap_select(cam_num)
+        cam_sel = cam_check(cam_num)
         if(cam_sel == True):
             cam_right_num = cam_num
             print("Select " + str(cam_right_num) + " As Camera Right(>>)")
             break
     if(cam_right_num == -1):
         print("Camera Right Not Available")
-    
-
+    return cam_left_num, cam_right_num
 
 if __name__ == '__main__':
-    main()
+    cam_left_num, cam_right_num = cap_select()
