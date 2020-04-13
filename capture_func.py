722c0629ed9de12d6b926b29c2a1d842a11f2876
astlive <irandomsky@gmail.com>
2020-04-13 14:54:25 +0800

    extend function

--- capture_func.py
+++ capture_func.py
@@ -1,15 +1,124 @@
 import os
 import cv2
 import time
+import numpy as np
+import imutils
 
-def sel_cap(cap_num):
+def cv_size(img):
+    return tuple(img.shape[1::-1])
+
+def init_cap(cap_index, width, height, fps):
+    cap = None
+    cap = cv2.VideoCapture(cap_index)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    cap.set(cv2.CAP_PROP_FPS, fps)
+    print("init VideoCapture on width:" + str(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) + " height:" + str(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), " fps:" + str(cap.get(cv2.CAP_PROP_FPS)))
+    return cap
+
+def sel_cap(skip = -1):
+    for i in range(10):
+        cap = init_cap(i, 640, 480, 60)
+        if(cap is None or not cap.isOpened() or i == skip):
+            continue
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret:
+                cv2.putText(frame, "cv2.CAP_PROP_FPS:" + str(cap.get(cv2.CAP_PROP_FPS)), (10,40), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0), 2)
+                cv2.imshow("Camera:" + str(i), frame)
+                key = cv2.waitKey(1)
+                if(key == ord('Y') or key == ord('y')):
+                    return i
+                elif(key == ord('N') or key == ord('n')):
+                    break
+        cap.release()
+        cv2.destroyAllWindows()
+    return None
+
+def mp_cap_worker(cap_ind, width, height, fps, q):
+    import cv2
+    cap = init_cap(cap_ind, width, height, fps)
+    print("mp_cap_worker Process:" + str(os.getpid()) + " cap_ind:" + str(cap_ind))
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if(ret):
+            if(cv_size(frame) != (width,height)):
+                frame = imutils.resize(frame, width=width, height=height)
+            q.put(frame)
+            cv2.waitKey(10)
+
+def test_cap(width,height,fps,cap_num,demo = False):
+    if(demo is False):
+        cap_ind1 = sel_cap()
+    else:
+        cap_ind1 = "./demo/gopro8(1).MP4"
+
     if(cap_num == 1):
-        for i in range(10):
-            cap = cv2.VideoCapture(i)
-            
+        cap_1 = init_cap(cap_ind1, width, height, fps)
+        while(cap_1.isOpened()):
+            ret, frame = cap_1.read()
+            if(ret):
+                if(cv_size(frame) != (width,height)):
+                    frame = imutils.resize(frame, width=width, height=height)
+                cv2.putText(frame, "Camera:" + str(cap_ind1), (10,40), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0), 2)
+                cv2.imshow("Capture", frame)
+                key = cv2.waitKey(1)
+                if(key == ord('Q') or key == ord('q')):
+                    break
+        cap_1.release()
+    elif(cap_num == 2):
+        import multiprocessing as mp
+        if(demo is False):
+            cap_ind2 = sel_cap(cap_ind1)
+        else:
+            cap_ind2 = "./demo/gopro8(2).MP4"
 
-def test_cap(height,width,fps,cap_num,show_msg = False):
-    if(show_msg):print()
+        imgq1 = mp.Manager().Queue()
+        imgq2 = mp.Manager().Queue()
+        w1 = mp.Process(target=mp_cap_worker, args=(cap_ind1,width,height,fps,imgq1,))
+        w2 = mp.Process(target=mp_cap_worker, args=(cap_ind2,width,height,fps,imgq2,))
+        w1.start()
+        w2.start()
+
+        no_signal_img = cv2.imread("./demo/no_signal.png")
+        cv2.resize(no_signal_img, dsize=(width,height))
+        img1 = no_signal_img
+        img2 = no_signal_img
+        
+        while (True):
+            FPS_count_start_time = time.time()
+            show_frame = False
+            if(imgq1.qsize() > 0 or imgq2.qsize() > 0):
+                show_frame = True
+
+            if(imgq1.qsize() > 0):
+                detect_time_start = time.time()
+                img1 = imgq1.get()
+            else:
+                cv2.putText(img1, "X", (10,40), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,0,255), 2)
+
+            if(imgq2.qsize() > 0):
+                img2 = imgq2.get()
+            else:
+                cv2.putText(img2, "X", (10,40), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,0,255), 2)
+            
+            if(show_frame):
+                if(img1.shape != img2.shape):
+                    img1 = cv2.resize(img1, dsize=(width,height))
+                    img2 = cv2.resize(img2, dsize=(width,height))
+                frame = np.hstack((img1, img2))
+                if(demo):print("img1.shape:" + str(img1.shape) + " img2.shape:" + str(img2.shape))
+                if(demo):print("frame.shape:" + str(frame.shape))
+                cv2.imshow("Capture", frame)
+            if(demo):print("imgq1.qsize():" + str(imgq1.qsize()) + " imgq2.qsize():" + str(imgq2.qsize()))
+            key = cv2.waitKey(1)
+            if(key == ord('q')):
+                break
+            elif(key == ord('p')):
+                key = cv2.waitKey(0)
+        w1.terminate()
+        w2.terminate()
+
 
 if __name__ == "__main__":
     resolution = input("Select resolution (1)640x480 (2)1280x720 (3)1920x1080 (4)manual input:")
